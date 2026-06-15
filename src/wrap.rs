effebf6fa6fddc12985768b6a85a146a81c86036
Navneet Aman <contact@navneetaman.com>
2026-06-15 14:17:52 +0530

    add tests.

--- src/wrap.rs
+++ src/wrap.rs
@@ -12,3 +12,18 @@ impl<T, E> WrappedResult<E> for T {
         Ok(self)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{WrappedOption, WrappedResult};
+    #[test]
+    fn t_wrapped_option() {
+        fn takes_option(_: Option<i32>){}
+        takes_option(4i32.some())
+    }
+    #[test]
+    fn t_wrapped_result() {
+        fn takes_result(_: Result<i32, ()>){}
+        takes_result(4i32.ok())
+    }
+}
