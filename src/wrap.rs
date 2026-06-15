e231e94d4713e98916a01709e96cbae4eb983ed3
Navneet Aman <contact@navneetaman.com>
2026-06-15 14:25:00 +0530

    return wrapped error.

--- src/wrap.rs
+++ src/wrap.rs
@@ -12,10 +12,18 @@ impl<T> WrappedResult for T {
         Ok(self)
     }
 }
+pub trait WrappedErrResult {
+    fn err<T>(self) -> Result<T, Self> where Self: std::marker::Sized;
+}
+impl<E> WrappedErrResult for E {
+    fn err<T>(self) -> Result<T, Self> where Self: std::marker::Sized {
+        Err(self)
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::{WrappedOption, WrappedResult};
+    use crate::{WrappedOption, WrappedResult, wrap::WrappedErrResult};
     #[test]
     fn t_wrapped_option() {
         fn takes_option(_: Option<i32>){}
@@ -30,5 +38,15 @@ mod tests {
     fn t_wrapped_result2() {
         fn returns_result(i: i32) -> Result<i32, ()>{ i.ok() }
         assert_eq!(Ok(4), returns_result(4))
+    }
+    #[test]
+    fn t_wrapped_err_result() {
+        fn takes_result(_: Result<(), i32>){}
+        takes_result(4i32.err())
+    }
+    #[test]
+    fn t_wrapped_err_result2() {
+        fn returns_result(i: i32) -> Result<(), i32>{ i.err() }
+        assert_eq!(Err(4), returns_result(4))
     }
 }
